1f311da7c35b737d4279279bfb16735acebb043e
Stiven Garzon <usg210802@outlook.com>
2025-04-27 16:32:41 -0500

    Creacion de menu desplegable

--- renderer.js
+++ renderer.js
@@ -2,10 +2,19 @@ import { actualizarHora } from "./funciones/fecha.js";
 
 actualizarHora();
 
+const btnInicio = document.getElementById('btn-logo');
+const menu = document.querySelector('.menu-desplegable');
+
+btnInicio.addEventListener('click', () =>{
+  let display = menu.style.display === 'none' ? 'block' : 'none';
+  menu.style.display = display;
+})
+
 const btnChrome = document.getElementById('btn-chrome');
 const btnBlock = document.getElementById('btn-block');  
 const btnTerminal = document.getElementById('btn-terminal');
 const btnCalc = document.getElementById('btn-calculadora');
+
 
 btnBlock.addEventListener('click', () => {
   // Enviar un mensaje al proceso principal para abrir el Bloc de Notas
@@ -28,3 +37,4 @@ btnCalc.addEventListener('click', () => {
   window.electron.abrirCalculadora();
   console.log('Intentando abrir Calcualadora');
 });
+
