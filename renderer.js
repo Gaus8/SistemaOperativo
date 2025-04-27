0261301f0118845cb685ac0632e430e72cd61136
Stiven Garzon <usg210802@outlook.com>
2025-04-27 18:01:06 -0500

    Creacion de panel de apagar, y  cerrar sesion, implementacion de metodos para el apagado y el cierre de sesion

--- renderer.js
+++ renderer.js
@@ -1,20 +1,48 @@
 import { actualizarHora } from "./funciones/fecha.js";
 
 actualizarHora();
-
+//Variables de DOm
+const btnPower = document.getElementById('btn-power');
+const menuApagar = document.querySelector('.menu-apagar');
 const btnInicio = document.getElementById('btn-logo');
 const menu = document.querySelector('.menu-desplegable');
+//APAGAR,CERRAR SESION, SUSPENDER
+const btnApagar = document.getElementById('btn-apagar');
+const btnCerrarSesion = document.getElementById('btn-cerrar-sesion')
 
-btnInicio.addEventListener('click', () =>{
-  let display = menu.style.display === 'none' ? 'block' : 'none';
-  menu.style.display = display;
-})
-
+//Variables de iconos
 const btnChrome = document.getElementById('btn-chrome');
 const btnBlock = document.getElementById('btn-block');  
 const btnTerminal = document.getElementById('btn-terminal');
 const btnCalc = document.getElementById('btn-calculadora');
 
+//METODOS PARA OCULTAR DIVS
+
+btnInicio.addEventListener('click', () => {
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+});
+
+btnPower.addEventListener('click', () => {
+  if (menuApagar.style.display === 'block') {
+    menuApagar.style.display = 'none';
+  } else {
+    menuApagar.style.display = 'block';
+  }
+});
+
+btnApagar.addEventListener('click', () =>{
+  window.electron.apagarSO();
+})
+
+btnCerrarSesion.addEventListener('click', () =>{
+  window.electron.cerrarSO();
+})
+
+//METODOS PARA INVOCAR ELEMENTOS
 
 btnBlock.addEventListener('click', () => {
   // Enviar un mensaje al proceso principal para abrir el Bloc de Notas
