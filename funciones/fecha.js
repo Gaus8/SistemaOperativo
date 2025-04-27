7b05977bcbdc4e6fc4f1b7fc19c7c18620c41144
Stiven Garzon <usg210802@outlook.com>
2025-04-27 11:44:17 -0500

    Creacion de carpeta funciones

---
+++ funciones/fecha.js
@@ -0,0 +1,13 @@
+export function actualizarHora() {
+  const ahora = new Date();
+
+  const diaNombre = ahora.toLocaleString('es-ES', { weekday: 'long' });  // Día de la semana en español
+  const diaNumero = ahora.getDate();  // Número del día
+  const mesNombre = ahora.toLocaleString('es-ES', { month: 'short' });  // Nombre del mes en español
+  const horaCompleta = ahora.toLocaleTimeString('es-ES');  // Hora en formato 24h
+
+  document.getElementById('fecha-sistema').textContent = 
+    `${diaNombre}, ${diaNumero} de ${mesNombre} - ${horaCompleta}`;
+}
+
+setInterval(actualizarHora, 1000);
--- renderer.js
+++ renderer.js
@@ -1,14 +1,3 @@
-function actualizarHora() {
-  const ahora = new Date();
+import { actualizarHora } from "./funciones/fecha.js";
 
-  const diaNombre = ahora.toLocaleString('es-ES', { weekday: 'long' });  // Día de la semana en español
-  const diaNumero = ahora.getDate();  // Número del día
-  const mesNombre = ahora.toLocaleString('es-ES', { month: 'short' });  // Nombre del mes en español
-  const horaCompleta = ahora.toLocaleTimeString('es-ES');  // Hora en formato 24h
-
-  document.getElementById('fecha-sistema').textContent = 
-    `${diaNombre}, ${diaNumero} de ${mesNombre} - ${horaCompleta}`;
-}
-
-setInterval(actualizarHora, 1000);
 actualizarHora();
